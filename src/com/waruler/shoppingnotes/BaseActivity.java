9ff2b0efa728d5a6b5f40eac579e474f5fad91bf
waruler <zhangkun_818@163.com>
2016-04-14 19:52:44 +0800

    set windowSoftInputMode
    set windowSoftInputMode to resize

--- src/com/waruler/shoppingnotes/BaseActivity.java
+++ src/com/waruler/shoppingnotes/BaseActivity.java
@@ -4,7 +4,6 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class BaseActivity extends Activity {
